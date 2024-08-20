b92e2066e477beb245830e57f769ab965566ad91
PatPat <126310196+kirkpatpat@users.noreply.github.com>
2024-08-20 13:51:57 +0800

    Changed api url

--- app/item/[id]/page.tsx
+++ app/item/[id]/page.tsx
@@ -23,7 +23,7 @@ const ItemDetailPage = () => {
 
   useEffect(() => {
     if (id) {
-      axios.get(`http://127.0.0.1:5000/api/items/${id}`)
+      axios.get(`https://nextjs-crud-pearl.vercel.app/api/items/${id}`)
         .then(response => {
           setItem(response.data);
           setUpdatedName(response.data.name);
